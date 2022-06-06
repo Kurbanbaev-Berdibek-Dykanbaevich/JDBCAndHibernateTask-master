e0195bdbb8c89fe17207c0f745a11dbbf4f6e623
berdibekkurbanbaev <kurbanbaevberdibek@gmail.com>
2022-06-06 15:30:08 +0300

    Initial commit

---
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/Main.java
@@ -0,0 +1,26 @@
+package peaksoft;
+
+
+import peaksoft.dao.UserDao;
+import peaksoft.dao.UserDaoJdbcImpl;
+import peaksoft.model.User;
+import peaksoft.service.UserServiceImpl;
+
+public class Main {
+    public static void main(String[] args) {
+        // реализуйте алгоритм здесь
+//        Util.connection();
+        UserDao userDao = new UserDaoJdbcImpl();
+//        userDao.createUsersTable();
+//        userDao.saveUser("Johny","Depp",(byte)45);
+//        userDao.saveUser("Daniel","Radcliffe",(byte)30);
+//        userDao.saveUser("Ron","Weasley",(byte)29);
+//        userDao.saveUser("Emma","Watson",(byte)18);
+//        userDao.dropUsersTable();
+//        System.out.println(userDao.getAllUsers());
+//        userDao.removeUserById(3);
+//        userDao.cleanUsersTable();
+//
+
+    }
+}
---
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/util/Util.java
@@ -0,0 +1,22 @@
+package peaksoft.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    private static final String URL = "jdbc:postgresql://localhost:5432/user_home_task";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "postgres";
+
+    public static Connection connection(){
+        Connection connection = null;
+        try{
+            connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+            System.out.println("Successfully connected to Database!!!");
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
